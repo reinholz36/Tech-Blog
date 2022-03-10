ecd24870eb3095974d2cb46bde44760a4792667d
reinholz36 <dreinholz28@gmail.com>
2022-03-10 13:16:39 -0500

    added comment, post, and index controller

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+//returns all comments from db
+router.get('/', (req, res) => {
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//enters comment into db
+router.post('/', withAuth, (req,res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
+//delet comment from db
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'No comment found with this id!' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
